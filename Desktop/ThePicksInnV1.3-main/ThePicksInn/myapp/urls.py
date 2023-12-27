2f186f353bcbcb01a4d29eacf5add2337e9ef55e
bsef19m513 <bsef19m513@pucit.edu.pk>
2023-12-27 16:20:11 +0500

    top 20 and leaderboard apis

--- Desktop/ThePicksInnV1.3-main/ThePicksInn/myapp/urls.py
+++ Desktop/ThePicksInnV1.3-main/ThePicksInn/myapp/urls.py
@@ -4,8 +4,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.home),
-    path('drinks/', views.drink_list),
-    path('drinks/<int:id>', views.drink_detail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
